45d721a9553d8fa7e5042686a27426cb3e3c1525
simeon <48757998+SimeonPr@users.noreply.github.com>
2019-05-11 17:44:39 +0200

    richtige Tabelle

--- HA2/numTabelle.m
+++ HA2/numTabelle.m
@@ -1,36 +1,35 @@
 T = zeros(12,6);
 for i = 1:12
-    %Nötig Werte berechnen
-    A = hilbert(i+1);
-    
+    %Nötige Werte berechnen
+    A = hilb(i+1);
+    b = A(:,1)+A(:,i+1);
     x = zeros(i+1,1);
     x(1)=1;
     x(i+1)=1;
-    
-    b = A(:,1)+A(:,i+1);
+    normx=norm(x,inf);
     
     x_bs=A\b;
     
     [L,R,P] = lu(A);
     b_perm = P*b;
-    y_lr = (b_perm\L).';
-    x_lr = (y_lr\R).';
+    y_lr = L\b_perm;
+    x_lr = R\y_lr;
     
     R = chol(A);
-    y_chol = (b\R.').';
-    x_chol = (y_chol\R).';
+    y_chol = R.'\b;
+    x_chol = R\y_chol;
     
-    %Erste Spalte
+    %Erste Spalte: Zahlen n von 2 bis 13
     T(i,1) = i+1;
-    %Zweite Spalte
+    %Zweite Spalte: Konditionszahlen der n-Hilbertmatrizen
     T(i,2) = cond(A,inf)*10^(-16);
-    %Dritte Spalte
-    T(i,3) = (norm(x-x_bs,inf))/(norm(x,inf));
-    %Vierte Spalte
-    T(i,4) = (norm(x-x_lr,inf))/(norm(x,inf));
-    %Fünfte Spalte
-    T(i,5) = (norm(x-x_chol,inf))/(norm(x,inf));
-    %Sechste Spalte
+    %Dritte Spalte: relative Fehler von x_bs
+    T(i,3) = (norm(x-x_bs,inf))/(normx);
+    %Vierte Spalte: relative Fehler von x_lr
+    T(i,4) = (norm(x-x_lr,inf))/(normx);
+    %Fünfte Spalte: relative Fehler von x_chol
+    T(i,5) = (norm(x-x_chol,inf))/(normx);
+    %Sechste Spalte: Probewert
     T(i,6) = norm(A*x_chol-b,inf);
 end
 disp(T)
