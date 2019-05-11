d7c811096cd7f886bfdf9e9064a5dc1f01a0b11f
simeon <48757998+SimeonPr@users.noreply.github.com>
2019-05-11 13:54:57 +0200

    Funktioniert jetzt

--- HA2/tridiagloes.m
+++ HA2/tridiagloes.m
@@ -1,17 +1,37 @@
-function [outputArg1,outputArg2] = tridiagloes(a,b,f)
-%UNTITLED7 Summary of this function goes here
-%   Detailed explanation goes here
+function [u] = tridiagloes(a,b,f)
+%Tridiagonalmatrix Summary of this function goes here
+%   Lösung von Au=f wobei A eine symmetrische positiv definite Tridiagonalmatrix ist. 
 dim = size(a,1);
-b(dim,:) = [0];
 c = zeros(dim,1);
-d = zeros(dim,1);
-temp = 0;
-for i = 1:dim
-    c(i) = root(a(i)-temp);
-    d(i) = b(i)/c(i);
-    temp = temp + d(i)^2;
+d = zeros(dim-1,1);
+
+%Für n=1
+c(1)=sqrt(a(1));
+d(1)=b(1)/c(1);
+%Für n=2...Dimension-1
+for n = 2:(dim-1)
+    c(n) = sqrt(a(n)-d(n-1)^2);
+    d(n) = b(n)/c(n);
 end
-d(dim,:) = [];
+%Für n=Dimension
+c(dim)=sqrt(a(dim)-d(dim-1)^2);
+%Ly=f lösen:
+y = zeros(dim,1);
+%Für n=1
+y(1) = f(1)/c(1);
+%Für n=2...dim
+for n=2:dim
+    y(n)=(f(n)-y(n-1)*d(n-1))/(c(n));
+end
+
+%L(t)u=y lösen:
+u = zeros(dim,1);
+%Für n=dim
+u(dim)=y(dim)/c(dim);
+%Für n=dim-1..1
+for n=dim-1:-1:1
+    u(n)=(y(n)-u(n+1)*d(n))/(c(n));
+end
 
 end
 
